2ed6ca0d103f25feac1af74121c17fb18b380c9a
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-04-30 21:54:31 +0200

    draw a square, circle and a triangle

--- drawsquare.py
+++ drawsquare.py
@@ -2,19 +2,43 @@ import turtle
 
 
 def draw_square():
+    brad_square = turtle.Turtle()
+    brad_square.shape("turtle")
+    brad_square.color("green")
+    brad_square.speed(3)
+
+    for i in range(1, 5):
+        brad_square.forward(100)
+        brad_square.right(90)
+
+
+def draw_circle():
+    pit_circle = turtle.Turtle()
+    pit_circle.shape("triangle")
+    pit_circle.color("white")
+
+    pit_circle.circle(100)
+
+
+def draw_triangle():
+    fred_triangle = turtle.Turtle()
+    fred_triangle.shape("circle")
+    fred_triangle.color("blue")
+
+    for i in range(0, 3):
+        fred_triangle.forward(100)
+        fred_triangle.left(120)
+
+
+def draw_shapes():
     window = turtle.Screen()
     window.bgcolor("red")
-
-    brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
+    window.title("My different shapes!")
+    draw_square()
+    draw_circle()
+    draw_triangle()
 
     window.exitonclick()
 
 
-draw_square()
+draw_shapes()
